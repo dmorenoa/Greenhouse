b5dd1dd2bdfd83ca2465fbc374060769598c5157
dmorenoa <danielmorenoa@outlook.com>
2018-08-22 15:03:02 -0500

    asdf

--- src/main.ino
+++ src/main.ino
@@ -3,14 +3,14 @@
 #include <relay.h>
 
 #define serialrate 115200
-#define pinlm35 0 
-#define relaypin 50
+int pinlm35 = 0;
+int relaypin = 50;
 
 void setup() {
     Serial.begin(serialrate);
     LM35.pinsetup(pinlm35);
     //analogReference(INTERNAL);
-    relay.pinsetup(uint8_t relaypin);
+    relay.pinsetup(relaypin);
 }
 
 void loop(){
