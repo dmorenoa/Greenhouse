fae7e41161405970253196fed75475e22c0dc8e1
dmorenoa <danielmorenoa@outlook.com>
2018-09-10 16:08:35 -0500

    Actualizacion 3pm 10 sep

--- lib/com/com.cpp
+++ lib/com/com.cpp
@@ -40,15 +40,80 @@ bool Com::read(){
 
 uint8_t Com::task(uint8_t vec){
     uint8_t taskvec[len/2];
-        for(int i=0;i<len; i++){
+    uint8_t numvec[len/2];
+        for(float i=0;i<len; i++){
             if(i%2==0){
-                continue;
+                numvec[i/2]=len[i];
             }
-        else{
-            taskvec[i/2-0.5] = len[i];
+            else{
+                taskvec[i/2-0.5] = len[i];
         }
     }
-    return taskvec;
+    return taskvec,numvec;
+}
+
+uint8_t Com::taskexec(uint8_t vect, uint8_t numv){
+    for(int i=0;i<len/2;i++){
+        uint8_t tarea = vect[i];
+        if(tarea == 0x50 ){
+            try{
+                LM35::LM35(numv[i]);
+            }
+            catch{
+                numv[i]= 0x00;
+            }
+            numv[i]= 0x01;
+        }
+        else if(tarea == 0x51 ){
+            try{
+                uint8_t temp = LM35::gettemp();
+            }
+            catch{
+                numv[i]= 0x00;
+            }
+            numv[i]= temp;
+        }
+        else if(tarea == 0x52 ){
+            try{
+                Relay::Relay(numv[i]);
+            }
+            catch{
+                numv[i]= 0x00;
+            }
+            numv[i]= 0x01;
+        }
+        else if(tarea == 0x53 ){
+            try{
+                Relay::on() ;
+            }
+            catch{
+                numv[i]= 0x00;
+            }
+            numv[i]= 0x01;
+        }
+        else if(tarea == 0x54 ){
+            try{
+                Relay::off() ;
+            }
+            catch{
+                numv[i]= 0x00;
+            }
+            numv[i]= 0x01;
+        }
+        else if(tarea == 0x55 ){
+            try{
+                Relay::swap() ;
+            }
+            catch{
+                numv[i]= 0x00;
+            }
+            numv[i]= 0x01;
+        }
+        else{
+
+        }
+    }
+    return numv;
 }
 
 int8_t Com::calCheckSum(){
@@ -57,6 +122,21 @@ int8_t Com::calCheckSum(){
         sum+=buffer[i];
     }
     return byte(0xFF-sum);
+}
+
+bool Com::send(uint8_t vect, uint8_t numv, uint8_t dest){
+    inp.write(0x01);
+    inp.write(0x03);  //origen= 0x003 -> Arduino
+    inp.write(dest);
+    inp.write(len);
+    for(int i=0;i<len/2;i++){
+        inp.write(vect[i]);
+        inp.write(numv[i]);
+        buffer[2*i] = vect[i];
+        buffer[2*i+1] = numv[i];
+    }
+    inp.write(calCheckSum());
+    clear();
 }
 
 bool Com::checkOrigin(uint8_t ori){
--- lib/com/com.h
+++ lib/com/com.h
@@ -7,9 +7,14 @@ class Com{
     public:
         Com();
         bool read();
+        bool send(uint8_t vect, uint8_t numv,uint8_t dest);
         bool checkOrigin(uint8_t ori);
         bool checkDestination(uint8_t des);
         uint8_t task(uint8_t vec);
+        uint8_t taskexec(uint8_t vect, uint8_t numv);
+        uint8_t len;
+        uint8_t inp_buffer[90];
+        uint8_t inp_data;
     private:
         int8_t calCheckSum();
         bool relaystate;
@@ -18,9 +23,6 @@ class Com{
         uint8_t buffer[90];
         uint8_t origen;
         uint8_t destino;
-        uint8_t len;
-        uint8_t inp_buffer[90];
-        uint8_t inp_data;
 };
 
 #endif
